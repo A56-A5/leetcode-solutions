7c0e57a07bf74e412505680f21d55490174893b5
ALVI A V <149242620+A56-A5@users.noreply.github.com>
2025-05-10 19:47:30 +0530

    Create Add Two Numbers.java

---
+++ medium/Linked List/Add Two Numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode(0);
+        ListNode res = ans;int total = 0,carry = 0;
+        while(l1 != null || l2!=null || carry !=0){
+            total = carry;
+            if(l1 != null){
+                total += l1.val;
+                l1 = l1.next;
+            }if(l2 != null){
+                total += l2.val;
+                l2 = l2.next;
+            }int num = total %10;
+            carry = total/10;
+            ans.next = new ListNode(num);
+            ans = ans.next;
+        }return res.next;
+    }
+}
