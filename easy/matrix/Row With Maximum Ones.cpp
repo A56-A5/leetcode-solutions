64fef5af4135d6c426d187ab3480d256a9e034ca
ALVI A V <149242620+A56-A5@users.noreply.github.com>
2025-05-11 19:13:24 +0530

    Create Row With Maximum Ones.cpp

---
+++ easy/matrix/Row With Maximum Ones.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        int row = 0, c = 0;
+
+        for (int i = 0; i < mat.size(); ++i) {
+            int ones = 0;
+            for (int j = 0; j < mat[i].size(); ++j) {
+                if (mat[i][j] == 1) {
+                    ones++;
+                }
+            }
+            if (ones > c) {
+                c = ones;
+                row = i;
+            }
+        }
+        return {row, c};
+    }
+};
