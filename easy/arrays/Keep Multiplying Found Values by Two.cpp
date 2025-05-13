b429e3e7a4e1d17a87087d8a6d48fbfc420460e1
ALVI A V <149242620+A56-A5@users.noreply.github.com>
2025-05-13 15:33:55 +0530

    Create Keep Multiplying Found Values by Two.cpp

---
+++ easy/arrays/Keep Multiplying Found Values by Two.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        while(true){
+            int flag = 0;
+            for(int i=0;i<nums.size();i++){
+                if(nums[i] == original){
+                    original *= 2;
+                    flag = 1; break; 
+                }
+            }if(flag == 0) break;
+        }return original;
+    }
+};
