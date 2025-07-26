0e6a8bf0f544ef14be93bd96addabd3559cebb3b
Alvi A V <149242620+A56-A5@users.noreply.github.com>
2025-07-26 14:20:31 +0530

    Update Circular Sentence.py

--- easy/string/Circular Sentence.py
+++ easy/string/Circular Sentence.py
@@ -1,9 +1,7 @@
 class Solution(object):
-    def isCircularSentence(self, sentence):
-        words = sentence.split(" ")  
-        for i in range(len(words)):
-            This = words[i]
-            Next = words[(i + 1) % len(words)]  
-            if This[-1] != Next[0]: 
-                return False
-        return True
+    def isCircularSentence(self, s):
+        for i in range(len(s)):
+            if s[i] == ' ':
+                if s[i-1] != s[i+1]:
+                    return False
+        return s[0] == s[len(s)-1]
