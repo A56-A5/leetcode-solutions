3c2e92c71c757be0bd9b1f877ad3f908084a14a5
Alvi A V <149242620+A56-A5@users.noreply.github.com>
2025-07-26 14:21:25 +0530

    Create Circular Sentence.java

---
+++ easy/string/Circular Sentence.java
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isCircularSentence(string s) {
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]==' ')
+            {
+                if(s[i-1] != s[i+1])
+                {
+                    return false;
+                }
+            }
+        }
+        return s[0] == s[s.size()-1];
+    }
+};
