c0aa229ef4cc688b38b7519c53ce8810dcd4c56f
Alvi A V <149242620+A56-A5@users.noreply.github.com>
2025-08-06 16:04:36 +0530

    Create Sort Array By Parity.cpp

---
+++ easy/array/Sort Array By Parity.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        
+        for(int i=0,j=0;i<nums.size();i++){
+            if(nums[i]%2==0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }return nums;
+    }
+};
